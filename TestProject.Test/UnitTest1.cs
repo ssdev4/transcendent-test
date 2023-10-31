7029365bd10d90a71326eeefb5959aabe1b7f3ee
Umer Naseem <umardev95@gmail.com>
2023-10-30 19:04:18 -0700

    Added Tests

---
+++ TestProject.Test/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TestProject.Controllers;
+using TestProject.Models;
+using TestProject.Repository.Abstraction;
+using Xunit;
+
+public class NumberControllerTests
+{
+    [Fact]
+    public async Task Index_ReturnsAViewResult_WithAllNumbers()
+    {
+        var mockNumberService = new Mock<INumberService>();
+        var controller = new NumberController(mockNumberService.Object);
+
+        var result = await controller.Index();
+
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public async Task Calculate_WhenModelStateIsValid_RedirectsToIndex()
+    {
+        var mockNumberService = new Mock<INumberService>();
+        var controller = new NumberController(mockNumberService.Object);
+        var model = new NumberEntry();
+
+        controller.ModelState.Clear();
+        var result = await controller.Calculate(model) as RedirectToActionResult;
+
+        Assert.NotNull(result);
+        Assert.Equal("Index", result.ActionName);
+    }
+
+    [Fact]
+    public async Task Calculate_WhenModelStateIsNotValid_ReturnsViewWithModel()
+    {
+        var mockNumberService = new Mock<INumberService>();
+        var controller = new NumberController(mockNumberService.Object);
+        var model = new NumberEntry();
+        controller.ModelState.AddModelError("PropertyName", "Error Message");
+
+        var result = await controller.Calculate(model) as ViewResult;
+
+        Assert.NotNull(result);
+        Assert.Equal("Index", result.ViewName);
+        Assert.Same(model, result.Model);
+    }
+}
